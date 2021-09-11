0ccdc31309a9973b2cd533959dbf67ae593e5472
JoseMagIII <jose_maglaqueiii@dlsu.edu.ph>
2021-09-12 01:39:36 +0800

    Main menu added

---
+++ app/src/main/java/com/mobdeve/project/sibat/Menu.java
@@ -0,0 +1,40 @@
+package com.mobdeve.project.sibat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    private Button playBtn, quitBtn2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        playBtn = (Button) this.findViewById(R.id.playBtn);
+        quitBtn2 = (Button) this.findViewById(R.id.quitBtn);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        quitBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
